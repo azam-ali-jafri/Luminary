544d9fd8ad3a4a53a51794964ecb635b0fd71901
Azam <azamalijafrii@gmail.com>
2024-07-04 12:11:20 +0530

    main layout

---
+++ app/(dashboard)/_components/Logo.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+import React from "react";
+
+const Logo = () => {
+  return <h1 className="font-extrabold text-lg text-primary">Luminary</h1>;
+};
+
+export default Logo;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,13 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Poppins } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -18,7 +22,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={poppins.className}>{children}</body>
       </html>
     </ClerkProvider>
   );
