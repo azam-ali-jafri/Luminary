be9237146c7383d5a943b567a025f0d3fd774fe7
Azam <azamalijafrii@gmail.com>
2024-07-05 15:05:27 +0530

    dynamic layout

--- app/(dashboard)/_components/SidebarRoutes.tsx
+++ app/(dashboard)/_components/SidebarRoutes.tsx
@@ -1,16 +1,25 @@
 "use client";
 
-import { Compass, Layout } from "lucide-react";
+import { BarChart, Compass, Layout, List } from "lucide-react";
 import React from "react";
 import SidebarItem from "./SidebarItem";
+import { usePathname } from "next/navigation";
 
 const guestRoutes = [
   { icon: Layout, label: "Dashboard", href: "/" },
   { icon: Compass, label: "Browse", href: "/search" },
 ];
 
+const treacherRoutes = [
+  { icon: List, label: "Courses", href: "/teacher/courses" },
+  { icon: BarChart, label: "Analytics", href: "/teacher/analytics" },
+];
+
 const SidebarRoutes = () => {
-  const routes = guestRoutes;
+  const pathname = usePathname();
+
+  const isteacher = pathname?.includes("/teacher");
+  const routes = isteacher ? treacherRoutes : guestRoutes;
 
   return (
     <div className="flex flex-col w-full">
--- components/NavbarRoutes.tsx
+++ components/NavbarRoutes.tsx
@@ -1,12 +1,34 @@
 "use client";
 
 import { UserButton } from "@clerk/nextjs";
-import React from "react";
+import { usePathname } from "next/navigation";
+import { Button } from "./ui/button";
+import { LogOut } from "lucide-react";
+import Link from "next/link";
 
 const NavbarRoutes = () => {
+  const pathname = usePathname();
+
+  const isteacher = pathname?.startsWith("/teacher");
+  const isplayer = pathname?.includes("/chapter");
+
   return (
     <div className="flex gap-x-2 ml-auto">
-      <UserButton />
+      {isteacher || isplayer ? (
+        <Link href={"/"}>
+          <Button size={"sm"} variant={"ghost"}>
+            <LogOut className="size-4 mr-2" />
+            Exit
+          </Button>
+        </Link>
+      ) : (
+        <Link href={"/teacher/courses"}>
+          <Button size={"sm"} variant={"ghost"}>
+            Teacher Mode
+          </Button>
+        </Link>
+      )}
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 };
